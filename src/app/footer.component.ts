1866787c508ca62e3308f8689cedee67152d8454
Scott Cooper <scttcper@gmail.com>
2018-08-21 22:33:27 -0700

    docs: BIGGER

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -4,22 +4,14 @@ import { Component, VERSION } from '@angular/core';
   selector: 'app-footer',
   template: `
   <footer class="footer mb-4 mt-5">
-    <div class="mb-1">
-      <mdo-github-button
-        count="true"
-        user="typectrl"
-        repo="ngx-codemirror"
-        >
-      </mdo-github-button>
-    </div>
     Angular {{ version }}
     <br>
     Released under the
     <a href="https://github.com/typectrl/ngx-codemirror/blob/master/LICENSE">MIT</a> license
     -
-    <a href="https://github.com/typectrl/ngx-codemirror">View source</a>
+    <a href="https://github.com/typectrl/ngx-codemirror">View Source</a>
     <br>
-    Listed on <a href="https://angular.parts/package/@ctrl/ngx-codemirror">angular.parts</a>
+    Listed on <a href="https://angular.parts/package/@ctrl/ngx-codemirror">Angular.parts</a>
   </footer>
   `,
   styles: [
@@ -27,7 +19,7 @@ import { Component, VERSION } from '@angular/core';
       .footer {
         line-height: 2;
         text-align: center;
-        font-size: 70%;
+        font-size: 69%;
         color: #999;
         font-family: var(--font-family-monospace);
       }
