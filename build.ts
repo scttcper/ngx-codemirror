a4a65be94c0ef65e793ccb3fd87fd60722c2537e
Scott Cooper <scttcper@gmail.com>
2020-03-25 18:01:36 -0700

    fix: upgrade to angular 9.1.0

--- build.ts
+++ build.ts
@@ -1,19 +1,14 @@
-
-import { copySync } from 'fs-extra';
+import { copyFileSync } from 'fs';
 import { build } from 'ng-packagr';
 import { join } from 'path';
-import * as rimraf from 'rimraf';
 
 
 async function main() {
-  // cleanup dist
-  rimraf.sync(join(process.cwd(), '/dist'));
-
   await build({
     project: join(process.cwd(), '/src/lib/package.json')
   });
-  copySync('README.md', join(process.cwd(), 'dist/README.md'));
-  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
+  copyFileSync('README.md', join(process.cwd(), 'dist/README.md'));
+  copyFileSync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 main()
