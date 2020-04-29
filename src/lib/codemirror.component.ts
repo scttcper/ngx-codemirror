7297faeaa6d78b2ca3b14d7f1c29b7dc85c25592
Scott Cooper <scttcper@gmail.com>
2020-04-28 21:14:09 -0700

    fix: Revert "feat: Declare CodeMirror as external variable (#262)"
    This reverts commit 15eac6f842f92e0c65c8d820eafc55edcfbc839b.

--- src/lib/codemirror.component.ts
+++ src/lib/codemirror.component.ts
@@ -30,7 +30,9 @@ function normalizeLineEndings(str: string) {
 }
 
 declare var require: any;
-declare var CodeMirror: any;
+/* tslint:disable */
+var CodeMirror: any;
+/* tslint:enable */
 
 @Component({
   selector: 'ngx-codemirror',
